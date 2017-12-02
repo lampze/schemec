a77b51c06e6739adb4f4829825aa699d52d4059a
shirui <1229408499@qq.com>
2017-12-02 14:37:53 +0800

    加入了提示，写的更加模块化了，解决了cut命令错误提示

--- scc.sh
+++ scc.sh
@@ -9,9 +9,24 @@ procNAME(){
     done
     echo ${N[@]}
 }
-INNAME=\"${1}\"
-OUTNAME=\"$(echo $1 | cut -d . -f 1).boot\"
-DEPENDNAME=$(echo $* | cut -d ' ' -f 2-$#)
-DEPENDNAME=$(procNAME $DEPENDNAME)
 
-echo "(make-boot-file ${OUTNAME} '(\"scheme\" \"petite\") ${DEPENDNAME} ${INNAME})" | scheme -q
+
+scc(){
+    INNAME=$(procNAME $1)
+    OUTNAME=$(echo $1 | cut -d . -f 1).boot
+    OUTNAME=$(procNAME $OUTNAME)
+if [ $# -lt 2 ]; then
+    echo "(make-boot-file ${OUTNAME} '(\"scheme\" \"petite\") ${INNAME})" | scheme -q
+else
+    DEPENDNAME=$(echo $* | cut -d ' ' -f 2-$#)
+    DEPENDNAME=$(procNAME $DEPENDNAME)
+    echo "(make-boot-file ${OUTNAME} '(\"scheme\" \"petite\") ${DEPENDNAME} ${INNAME})" | scheme -q
+fi
+}
+
+
+if [ $# = 0 ]; then
+    echo "这是一个将chez scheme语言编译成boot文件的脚本，请在命令后面接上要编译的文件。"
+else
+    scc $*
+fi
